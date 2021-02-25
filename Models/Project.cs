e68d0c433a203b9bcfbfc1050afa0d0ad4a88dd1
Alejandro Maldonado <amwebdev9@outlook.com>
2021-02-25 13:42:42 -0600

    Add project files.

---
+++ Models/Project.cs
@@ -0,0 +1,46 @@
+﻿using BugTracker.Extensions;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BugTracker.Models
+{
+    public class Project
+    {
+        public Project()
+        {
+            Members = new HashSet<BTUser>();
+            Tickets = new HashSet<Ticket>();
+        }
+
+        public int Id { get; set; }
+        [Required]
+        [StringLength(50)]
+        [Display(Name = "Project Name")]
+        public string Name { get; set; }
+
+        [Display(Name = "Description")]
+        public string Description { get; set; }
+
+        [Display(Name = "Select Image")]
+        [NotMapped]
+        [DataType(DataType.Upload)]
+        [MaxFileSize(2 * 1024 * 1024)]
+        [AllowedExtensions(new string[] { ".jpg", ".png" })]
+        public IFormFile ImageFormFile { get; set; }
+        public string ImageFileName { get; set; }
+        public byte[] ImageFileData { get; set; }
+        public int? CompanyId { get; set; }
+
+
+        public virtual ICollection<BTUser> Members { get; set; }
+        //public IList<ProjectUser> ProjectUsers { get; set; }
+
+        public virtual ICollection<Ticket> Tickets { get; set; }
+        public virtual Company Company { get; set; }
+    }
+}
---
+++ Models/TicketAttachment.cs
@@ -0,0 +1,34 @@
+﻿using BugTracker.Extensions;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BugTracker.Models
+{
+    public class TicketAttachment
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Select Image")]
+        [NotMapped]
+        [DataType(DataType.Upload)]
+        [MaxFileSize(2 * 1024 * 1024)]
+        [AllowedExtensions(new string[] { ".jpg", ".png", ".doc", ".docx", ".xls", ".xlsx", ".pdf" })]
+        public IFormFile FormFile { get; set; }
+        public string FileName { get; set; }
+        public byte[] FileData { get; set; }
+
+
+        public string Description { get; set; }
+        public DateTimeOffset Created { get; set; }
+
+        public int TicketId { get; set; }
+        public virtual Ticket Ticket { get; set; }
+        public string UserId { get; set; }
+        public virtual BTUser User { get; set; }
+    }
+}
